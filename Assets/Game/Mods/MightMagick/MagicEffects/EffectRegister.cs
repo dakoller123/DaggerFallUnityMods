895f34af8e29dd6b07ebddc2c674e6bd8991a9e3
Koller Dávid Dániel <dkoller@geomant.com>
2021-07-15 11:05:28 +0200

    Rewrote savingthrows mechanic

--- Assets/Game/Mods/MightMagick/MagicEffects/EffectRegister.cs
+++ Assets/Game/Mods/MightMagick/MagicEffects/EffectRegister.cs
@@ -6,15 +6,12 @@ namespace MightyMagick.MagicEffects
     {
         private HealSpellPoints healSpellPoints;
         private ExtraSpellPts extraSpellPts;
-        private DamageHealth damageHealth;
         public void RegisterNewMagicEffects()
         {
             healSpellPoints = new HealSpellPoints();
             extraSpellPts = new ExtraSpellPts();
-            damageHealth = new DamageHealth();
             GameManager.Instance.EntityEffectBroker.RegisterEffectTemplate(healSpellPoints, true);
             GameManager.Instance.EntityEffectBroker.RegisterEffectTemplate(extraSpellPts, true);
-            GameManager.Instance.EntityEffectBroker.RegisterEffectTemplate(damageHealth, true);
         }
     }
 }
